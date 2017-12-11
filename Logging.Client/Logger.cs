ea97d857622270ab675be4cf00b3a2e83f0821f2
Steven Godin <sagodin@gmail.com>
2017-12-11 00:34:05 -0500

    Added documentation to the Logger class. Added unit tests for the Logger class.

--- Logging.Client/Logger.cs
+++ Logging.Client/Logger.cs
@@ -4,28 +4,47 @@ using Logging.Shared;
 
 namespace Logging.Client
 {
+	/// <summary>
+	/// A simple logging class to simply send log messages using a logging service.
+	/// </summary>
+	/// <remarks>
+	/// Allows you to call LogInfo, LogWarning, LogError to 
+	/// quickly log messages to the logging service, similar to log4net.
+	/// This logger will also catch any exceptions that the Logging service
+	/// might throw when a message is logged.
+	/// </remarks>
 	public class Logger
 	{
 		private readonly ILoggingService _loggingService;
 
+		/// <summary>Creates a new Logger.</summary>
+		/// <param name="loggingService">The Logging service to use.</param>
+		/// <param name="clientId">The initial ClientId to use.</param>
 		public Logger(ILoggingService loggingService, string clientId)
 		{
-			_loggingService = loggingService;
+			_loggingService = loggingService ?? throw new ArgumentNullException(nameof(loggingService));
 			ClientId = clientId;
 		}
 
+		/// <summary>The ClientId to use in all Log Messages.</summary>
 		public string ClientId { get; set; }
 
+		/// <summary>Sends an Info message to the logging service.</summary>
+		/// <param name="message">The message text to log.</param>
 		public void LogInfo(string message)
 		{
 			LogMessage(LogLevel.Info, message);
 		}
 
+		/// <summary>Sends a Warning message to the logging service.</summary>
+		/// <param name="message">The message text to log.</param>
 		public void LogWarning(string message)
 		{
 			LogMessage(LogLevel.Warning, message);
 		}
 
+		/// <summary>Sends an Error message to the logging service.</summary>
+		/// <param name="message">The message text to log.</param>
 		public void LogError(string message)
 		{
 			LogMessage(LogLevel.Error, message);
@@ -45,7 +64,6 @@ namespace Logging.Client
 			catch (Exception e)
 			{
 				Console.WriteLine("Failed to Log Message. ERROR: " + e.Message);
-				throw;
 			}
 		}
 	}
---
+++ Logging.Tests/Client/LoggerTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Logging.Client;
+using Logging.Services;
+using Logging.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Logging.Tests.Client
+{
+	[TestClass]
+	public class LoggerTest
+	{
+		private const string MESSAGE = "A Test Message";
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ConstructorThrowsExceptionWithNullLoggingService()
+		{
+			var logger = new Logger(null, "Test");
+		}
+
+		[TestMethod]
+		public void ConstructorSetsFieldsAndProperties()
+		{
+			const string clientId = "Test";
+			var loggingService = new Mock<ILoggingService>().Object;
+			var logger = new Logger(loggingService, clientId);
+
+			// Create PrivateObject of logger to check backing field.
+			var privateLogger = new PrivateObject(logger);
+
+			Assert.AreEqual(loggingService, privateLogger.GetField("_loggingService"));
+			Assert.AreEqual(clientId, logger.ClientId);
+		}
+
+		[TestMethod]
+		public void LogInfoCallsLogMessageWithInfoLevelAndMessage()
+		{
+			VerifyLoggerActionCallsLogMessageWithExpectedLevel
+				(l => l.LogInfo(MESSAGE), LogLevel.Info);
+		}
+
+		[TestMethod]
+		public void LogWarningCallsLogMessageWithWarningLevel()
+		{
+			VerifyLoggerActionCallsLogMessageWithExpectedLevel
+				(l => l.LogWarning(MESSAGE), LogLevel.Warning);
+		}
+
+		[TestMethod]
+		public void LogErrorCallsLogMessageWithErrorLevel()
+		{
+			VerifyLoggerActionCallsLogMessageWithExpectedLevel
+				(l => l.LogError(MESSAGE), LogLevel.Error);
+		}
+
+		private void VerifyLoggerActionCallsLogMessageWithExpectedLevel
+			(Action<Logger> loggerAction, LogLevel expectedLogLevel)
+		{
+			var mockLoggingService = new Mock<ILoggingService>();
+			var logger = new Logger(mockLoggingService.Object, "Test");
+
+			loggerAction.Invoke(logger);
+
+			// Verify the message object passed to LogMessage has the expected values
+			mockLoggingService.Verify(ls => ls.LogMessage(It.Is<LogMessage>(msg =>
+				msg.ClientId == logger.ClientId &&
+				msg.LogLevel == expectedLogLevel &&
+				msg.Message == MESSAGE)));
+		}
+
+		[TestMethod]
+		public void VerifyLoggerCatchesLoggingServiceException()
+		{
+			var mockLoggingService = new Mock<ILoggingService>();
+			var logger = new Logger(mockLoggingService.Object, "Test");
+			mockLoggingService.Setup(ls => ls.LogMessage(It.IsAny<LogMessage>()))
+				.Throws<Exception>();
+
+			logger.LogInfo(MESSAGE);
+
+			// Verify No Exception thrown
+			// Note that this test doesn't verify that the message was
+			// written to the console.
+		}
+	}
+}
